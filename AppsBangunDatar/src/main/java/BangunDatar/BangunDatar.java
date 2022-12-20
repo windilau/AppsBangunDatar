2d4d2cda3ee9dd08decb9b59383a61691a7b493d
windilau <windi.laudi@mhs.itenas.ac.id>
2022-12-20 10:49:24 +0700

    initiate commit project

---
+++ AppsBangunDatar/src/main/java/BangunDatar/BangunDatar.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package BangunDatar;
+
+/**
+ *
+ * @author Windi Laudi
+ */
+abstract class BangunDatar {
+
+    private String namaBangunDatar;
+
+    public BangunDatar() {
+    }
+
+    public BangunDatar(String namaBangunDatar) {
+        this.namaBangunDatar = namaBangunDatar;
+    }
+
+    public String getNamaBangunDatar() {
+        return namaBangunDatar;
+    }
+
+    public void setNamaBangunDatar(String namaBangunDatar) {
+        this.namaBangunDatar = namaBangunDatar;
+    }
+
+    abstract double luas();
+
+    abstract double keliling();
+}
